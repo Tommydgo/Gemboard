2be0aa89e995b4689b5d7796d73496e17799df72
Tommydgo <tommy.diago@epitech.eu>
2026-07-25 22:05:19 +0000

    feat: update API endpoint and redirect to login page

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,4 @@
+import path from 'path'
 import dotenv from 'dotenv'
 import express from 'express'
 import cors from 'cors'
@@ -14,13 +15,14 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../../frontend')))
 app.use('/', authRoutes)
 app.use('/', userRoutes)
 app.use('/', todoRoutes)
 app.use('/', boardRoutes)
 app.use('/', listsRoutes)
 app.get('/', (req, res) => {
-    res.send("API is running");
+    res.redirect('/login.html');
 });
 app.use(notFound)
 
